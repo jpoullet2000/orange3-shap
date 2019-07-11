973f7678a6689053fde9516453f6683ec9793017
Jean-Baptiste Poullet <jean-baptiste.x.poullet@gsk.com>
2019-07-11 15:30:51 +0200

    trying to use thread

--- orangecontrib/oshap/widgets/OWShapSingle.py
+++ orangecontrib/oshap/widgets/OWShapSingle.py
@@ -9,10 +9,11 @@ from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets import gui
 from Orange.widgets.widget import AttributeList
 from Orange.widgets.utils.webview import WebviewWidget
-
 from sklearn.ensemble.forest import RandomForestRegressor as SKL_RF
 from Orange.regression.random_forest import RandomForestRegressor
-
+from Orange.widgets.utils.concurrent import ThreadExecutor, FutureWatcher
+import concurrent.futures
+from AnyQt.QtCore import pyqtSlot
 
 class OWShapSingle(OWWidget):
     name = "Shap single plot"
@@ -31,11 +32,36 @@ class OWShapSingle(OWWidget):
         plt.tight_layout()
         self.max_nr_features = 20
         self.sample_index = 0
+
         box = gui.widgetBox(self.controlArea, "Info")
         self.infoa = gui.widgetLabel(box, 'No data on input yet, waiting to get something.')
         gui.widgetLabel(box, 'Sample index')
         self.sample_box = gui.spin(box, self, 'sample_index', 0, 0)
         self.button = gui.button(box, self, 'Regenerate plots', callback=self._on_click_button)
+
+        self._executor = ThreadExecutor()
+        self.is_ready = True
+
+
+    def turn_app_busy(self):
+        self.is_ready = False
+        self.progressBarInit()
+        self.button.setEnabled(False)
+        self.infoa.setText("Data are not rendered yet, please wait...")
+
+    def turn_app_ready(self):
+        self.button.setEnabled(True)
+        self.is_ready = True
+        self.progressBarFinished()
+        self.infoa.setText("Plots are up to date!")
+    
+    @pyqtSlot(float)
+    def setProgressValue(self, value):
+        self.progressBarSet(value)
+    
+    @pyqtSlot(concurrent.futures.Future)
+    def __update_is_done(self, f):
+        self.turn_app_ready()
 
     @Inputs.data
     def set_data(self, dataset):
@@ -44,7 +70,7 @@ class OWShapSingle(OWWidget):
         else:
             self.infoa.setText('No data on input yet, waiting to get something.')
         
-        self.dataset =  dataset
+        self.dataset = dataset
         self.sample_box.setMaximum(len(dataset.X) - 1)
 
     @Inputs.model
@@ -60,16 +86,38 @@ class OWShapSingle(OWWidget):
     def _on_click_button(self):
         self._update()
 
+
     def _update(self):
+        if self.is_ready:    
+            if self.dataset is None or self.model is None:
+                self.infoa.setText('No input data...')
+                return
+
+        plt.close('all')
+#        self.turn_app_busy()
+#        self.task = Task()
+#        self.task.future = self._executor.submit(self.__update)
+#        self.task.watcher = FutureWatcher(self.task.future)
+#        self.task.watcher.done.connect(self.__update_is_done)
+        self.__update()
+        
+    def __update(self):
         X = self.dataset.X
         idx = int(self.sample_index)
         model = self.model
         features = [feature.name for feature in self.dataset.domain.attributes]
         explainer = shap.TreeExplainer(model.skl_model)
         shap_values = explainer.shap_values(X)
-        plt.close('all')
         shap.force_plot(explainer.expected_value, shap_values[idx, :], X[idx, :], feature_names=features, matplotlib=True)
+    
 
+class Task:
+    """
+    Task class to perform computations in parallels
+    """
+    def __init__(self):
+        self.future = None
+        self.watcher = None
 
 if __name__ == "__main__":  # pragma: no cover
     data = Table('housing')
